9b7442372f4743b5a3a2b6c4ce5aefe489fc451c
solikear <919187569@qq.com>
2020-09-21 19:27:18 +0800

    初始化

---
+++ travel/src/pages/home/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <div>home</div>
+    <router-link to="/list">列表页</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style>
+.home {
+  font-size: 50px;
+}
+</style>
--- travel/src/router/index.js
+++ travel/src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-
+import Home from '@/pages/home/Home.vue'
+import List from "@/pages/list/List";
 Vue.use(VueRouter)
 
 const routes = [
@@ -11,13 +11,10 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/list',
+    name: 'List',
+    component: List
+  },
 ]
 
 const router = new VueRouter({
