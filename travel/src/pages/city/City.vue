258b29e3834e1c76e2070155ffdcae5c2a6677ac
solikear <919187569@qq.com>
2020-09-28 15:42:57 +0800

    finish city css

--- travel/src/pages/city/City.vue
+++ travel/src/pages/city/City.vue
@@ -2,18 +2,44 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
+    <city-list></city-list>
+    <city-alphabet></city-alphabet>
   </div>
 </template>
 
 <script>
 import CityHeader from './components/Header'
 import CitySearch from './components/Search'
+import CityList from './components/List'
+import CityAlphabet from './components/Alphabet'
+import axios from 'axios'
 
 export default {
   name: "City",
   components: {
     CityHeader,
     CitySearch,
+    CityList,
+    CityAlphabet,
+  },
+  mounted() {
+    axios.get("/api/city.json").then(this.getCityInfoSucc)
+  },
+  data() {
+    return {
+      hotCities:[],
+      cities:{},
+    }
+  },
+  methods: {
+    getCityInfoSucc(res) {
+      res = res.data
+      if(res.ret && res.data){
+        const data = res.data
+        this.hotCities = data.hotCities
+        this.cities = data.cities
+      }
+    }
   }
 }
 </script>
