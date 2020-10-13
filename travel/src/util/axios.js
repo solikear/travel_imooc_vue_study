f1dcebf43e2aab751dacda8c9482797d3f9fea7c
solikear <919187569@qq.com>
2020-10-13 16:46:47 +0800

    在axios响应拦截器加了部分错误提示

--- travel/src/util/axios.js
+++ travel/src/util/axios.js
@@ -14,6 +14,18 @@ let loadingInstance = null
 let msgOption = {
     offset: 0
 }
+
+let httpCode = {        //这里我简单列出一些常见的http状态码信息，可以自己去调整配置
+    400: '请求参数错误',
+    401: '权限不足, 请重新登录',
+    403: '服务器拒绝本次访问',
+    404: '请求资源未找到',
+    500: '内部服务器错误',
+    501: '服务器不支持该请求中使用的方法',
+    502: '网关错误',
+    504: '网关超时'
+}
+
 axios.interceptors.request.use(
     config => {
         loadingInstance = Loading.service({
@@ -32,32 +44,41 @@ axios.interceptors.request.use(
         loadingInstance.close()
         Message.error({
             message: '请求错误',
-            offset:0,
+            offset: 0,
         })
         return Promise.reject(err)
     }
 )
 
 axios.interceptors.response.use(res => {
-    if (typeof res.data !== 'object') {
-        // Toast.fail('服务端异常！')
-        return Promise.reject(res)
+        if (typeof res.data !== 'object') {
+            // Toast.fail('服务端异常！')
+            return Promise.reject(res)
+        }
+        console.log(res)
+        if (res.status === 200) {
+            loadingInstance.close()
+        } else {
+            loadingInstance.close()
+            Message.error("请求出错！" + res.status)
+        }
+        return res.data
+    }, (err) => {
+        if (err.response) {
+            // 根据请求失败的http状态码去给用户相应的提示
+            let tips = err.response.status in httpCode ? httpCode[err.response.status] : "请求出现错误！"
+            Message({
+                message: tips,
+                type: 'error'
+            })
+            loadingInstance.close()
+            // Message.error({
+            //     message: '请求失败，请稍后再试',
+            //     offset: 5,
+            // })
+        }
+        return Promise.reject(err)
     }
-    console.log(res)
-    if (res.status === 200) {
-        loadingInstance.close()
-    } else {
-        loadingInstance.close()
-        Message.error("请求出错！" + res.status)
-    }
-    return res.data
-}, (err) => {
-    loadingInstance.close()
-    Message.error({
-        message: '请求失败，请稍后再试',
-        offset:5,
-    })
-    return Promise.reject(err)
-})
+)
 
 export default axios
